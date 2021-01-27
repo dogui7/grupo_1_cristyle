1188d3747e29f54f12959592b233fb7bd17c1f08
francoAssaneo <francoassaneo@gmail.com>
2021-01-27 14:39:41 -0300

    Maquetado index.html

---
+++ cristyle/app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express ();
+
+const publicPath = path.resolve (__dirname, './public');
+
+app.use(express.static (publicPath));
+
+app.listen (3500, () => {
+    console.log ( 'Servidor Funcionando')
+} );
+
+app.get('/',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/index.html'))
+});
+
+/* app.get('/register',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/register.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/login.html'))
+}); */
