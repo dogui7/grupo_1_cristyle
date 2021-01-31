a8e49f23e2e8fb7af30f2720b7bd6dc6e0cddc64
dogui7 <guido.maimone7@gmail.com>
2021-01-31 19:19:56 -0300

    Comienzo de trabajo en el login

--- cristyle/app.js
+++ cristyle/app.js
@@ -17,10 +17,18 @@ app.get('/',(req,res)=>{
     res.sendFile (path.resolve (__dirname, './views/index.html'))
 });
 
-/* app.get('/register',(req,res)=>{
+app.get('/login',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/login.html'))
+});
+
+app.get('/register',(req,res)=>{
     res.sendFile (path.resolve (__dirname, './views/register.html'))
 });
 
-app.get('/login',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/login.html'))
-}); */
+app.get('/productCart',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/productCart.html'))
+});
+
+app.get('/productDetail',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/productDetail.html'))
+});
