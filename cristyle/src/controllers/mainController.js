3c686ce2ef6eedaa5d3cc4ede706d51da137b996
dogui7 <guido.maimone7@gmail.com>
2021-02-21 17:27:16 -0300

    Index con titulo de pestaña

--- cristyle/src/controllers/mainController.js
+++ cristyle/src/controllers/mainController.js
@@ -3,7 +3,8 @@ const path = require("path");
 module.exports = {
 
     index: (req, res) => {
-        let cssSheets = ["normalize", "footer", "header", "index"]
-        return res.render(path.resolve (__dirname, "../views/index.ejs"), {cssSheets})
+        let cssSheets = ["normalize", "footer", "header", "index"];
+        let title = "Sitio oficial de Cristyle";
+        return res.render(path.resolve (__dirname, "../views/index.ejs"), {cssSheets, title})
     }
 }
