cc74fc79b51a29ec361b44e820777c436537c78d
francoAssaneo <francoassaneo@gmail.com>
2021-03-04 12:42:15 -0300

    Multer

--- cristyle/src/routes/users.js
+++ cristyle/src/routes/users.js
@@ -1,7 +1,19 @@
 const express = require ("express");
 const router = express.Router();
+const multer = require ('multer');
 
 const usersController = require ("../controllers/usersController");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/images/usuarios'));    //Aquí deben indicar donde van a guardar la imagen
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'foto' + '-' + Date.now()+ path.extname(file.originalname));      
+    }
+  })
+   
+const upload= multer({ storage });
 
 router.get("/login", usersController.login);
 router.get("/register", usersController.register);
