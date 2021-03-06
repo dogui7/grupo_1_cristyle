ebfc3c224ca2458909287b8b08054f68127e7cc0
dogui7 <guido.maimone7@gmail.com>
2021-03-06 18:23:53 -0300

    Mejora al códigode productos en oferta al azar de index

--- cristyle/src/controllers/mainController.js
+++ cristyle/src/controllers/mainController.js
@@ -12,14 +12,16 @@ module.exports = {
 
         let discountProducts = products.getAll().filter(product => product.discount >= discountToFilter);
         let discountProductsToShow = [];
-        
+
         for (let i = 0; i < numberOfProducts; i++) {
+            if (discountProducts.length == 0) {break}; //Si no hay mas elementos, salimos del for
             discountProductsToShow.push( //Pusheamos al array que vamos a enviar...
                 discountProducts.splice( //Splice toma un elemento del array, el primer parametro determina la posicion, el segundo cuantos elementos
                     Math.floor(Math.random()*discountProducts.length), 1
-                )[0] // Ya que splice devuelve un array con 1 solo elemento en este caso, lo quitamos del array
+                )[0] // Ya que splice devuelve un array, lo quitamos del array
             );
         }
+
         return res.render(path.resolve (__dirname, "../views/index.ejs"), {cssSheets, title, discountProductsToShow})
     }
 }
