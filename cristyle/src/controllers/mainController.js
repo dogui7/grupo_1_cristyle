185d66eb0ea4c0e20ea889f74ad91a31e4a0762a
dogui7 <guido.maimone7@gmail.com>
2021-04-17 14:31:43 -0300

    Index con db

--- cristyle/src/controllers/mainController.js
+++ cristyle/src/controllers/mainController.js
@@ -1,28 +1,29 @@
-const path = require("path");
-const products = require ("../data/products/productsModel");
+const db = require ("../database/models");
 
 module.exports = {
-
     index: (req, res) => {
         let cssSheets = ["index", "showProducts"];
         let title = "Sitio oficial de Cristyle";
 
         let discountToFilter = 20;
-        let numberOfProducts = 4;
-
-        let discountProducts = products.getAll().filter(product => product.discount >= discountToFilter);
+        let numberOfProductsToShow = 4;
         let discountProductsToShow = [];
 
-        for (let i = 0; i < numberOfProducts; i++) {
-            if (discountProducts.length == 0) {break}; //Si no hay mas elementos, salimos del for
-            discountProductsToShow.push( //Pusheamos al array que vamos a enviar...
-                discountProducts.splice( //Splice toma un elemento del array, el primer parametro determina la posicion, el segundo cuantos elementos
-                    Math.floor(Math.random()*discountProducts.length), 1
-                )[0] // Ya que splice devuelve un array, lo quitamos del array
-            );
-        }
-
-        //return res.render(path.resolve (__dirname, "../views/index.ejs"), {cssSheets, title, discountProductsToShow})
-        return res.render("index.ejs", {cssSheets, title, discountProductsToShow})
+        db.Product.findAll() 
+        .then (function (products){
+            discountProducts = products.filter(product => product.discount >= discountToFilter);
+            for (let i = 0; i < numberOfProductsToShow; i++) {
+                if (discountProducts.length == 0) {break}; //Si no hay mas elementos, salimos del for
+                discountProductsToShow.push( //Pusheamos al array que vamos a enviar...
+                    discountProducts.splice( //Tomamos un elemento al azar del array. Splice toma un elemento del array, el primer parametro determina la posicion, el segundo cuantos elementos
+                        Math.floor(Math.random()*discountProducts.length), 1
+                    )[0] // Ya que splice devuelve un array, lo quitamos del array para poder pushearlo
+                );
+            }
+            return res.render("index.ejs", {cssSheets, title, discountProductsToShow})
+        })
+        .catch (error => {
+            res.send (error)
+        })
     }
 }
