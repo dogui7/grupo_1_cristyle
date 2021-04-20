79b4aa607916e1cfa525e2669edf4ff637690e37
dogui7 <guido.maimone7@gmail.com>
2021-04-20 18:26:17 -0300

    Agregados comentarios en middlewares > routes > users > loggedMiddleware.js / notLoggedMiddleware.js

--- cristyle/src/middlewares/routes/users/loggedMiddleware.js
+++ cristyle/src/middlewares/routes/users/loggedMiddleware.js
@@ -1,6 +1,8 @@
 function loggedMiddleware (req,res,next){
+    // Si el usuario SE ENCUENTRA logueado, continuar
     if (req.session && req.session.userLogged != undefined){
         next();
+    // Si el usuario NO SE ENCUENTRA logueado, redireccionar al inicio de sesión
     } else {
         res.redirect("/usuarios/iniciarSesion");
     }
--- cristyle/src/middlewares/routes/users/notLoggedMiddleware.js
+++ cristyle/src/middlewares/routes/users/notLoggedMiddleware.js
@@ -1,6 +1,8 @@
 function notLoggedMiddleware (req,res,next){
+    // Si el usuario NO SE ENCUENTRA logueado, continuar
     if (req.session.usuarioLogueado == undefined){
         next();
+    // Si el usuario SE ENCUENTRA logueado, redireccionar al index
     } else {
         res.redirect("/");
     }
