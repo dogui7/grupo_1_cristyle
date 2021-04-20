e520dda1766dddefe4b785e9fdb722cc8c4831da
dogui7 <guido.maimone7@gmail.com>
2021-04-20 15:56:48 -0300

    Agregados comentarios en app.js

--- cristyle/src/app.js
+++ cristyle/src/app.js
@@ -10,34 +10,47 @@ const cookieParser = require('cookie-parser');
 app.set("port", process.env.PORT || 3500);
 app.listen (app.get("port"), () => console.log ("Server running in http://localhost:" + app.get("port")));
 
-// Middlewares Aplication
+/************************ Middlewares application importados y nativos ************************/
+
+// Para poder utilizar los contenidos en public y en views respectivamente, sin tener que poner toda la ruta
 app.use(express.static (path.resolve (__dirname, "../public")));
 app.set('views', path.resolve(__dirname, './views'));
+// Para poder tomar la información que viene en el body de un form
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Para poder utilizar otros metodos ademas de GET y POST en las rutas y los forms
 app.use(methodOverride("_method"));
+// Para poder utilizar session
 app.use(session ({
     secret: "Secret",
     resave: true,
     saveUninitialized: true    
 }));
+// Para poder utilizar cookies
 app.use(cookieParser());
-app.use(userLoggedMiddleware);
-
-
-//View engine
+// View engine
 app.set("view engine", "ejs");
 
-// Routes
+/************************ Middlewares application hechos por nosotros ************************/
+
+// Checkea si alguien está logueado, ya sea por session o cookies, y lo pone en locals para  
+// acceder a la información del usuario logueado en cualquier parte del proyecto
+app.use(userLoggedMiddleware);
+
+/************************ Routes ************************/
+
+// Main routes
 const mainRouter = require("./routes/main");
 app.use("/", mainRouter);
 
+// Users routes
 const usersRouter = require("./routes/users");
 app.use("/usuarios", usersRouter);
 
+// Products routes
 const productsRouter = require("./routes/products");
 app.use("/productos", productsRouter);
 
-//Use this route to test things in routes /t/1, /t/2, etc
+// Usar esta ruta para testear código en las rutas /t/1, /t/2, etc
 const testsRouter = require("./routes/tests");
 app.use("/t", testsRouter);
