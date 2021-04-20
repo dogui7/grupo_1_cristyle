af24b4f84bcda2d2e89911ea91683233d8538af7
dogui7 <guido.maimone7@gmail.com>
2021-04-20 16:30:28 -0300

    Agregados comentarios en middlewares > application > userLoggedMiddleware.js

--- cristyle/src/middlewares/application/userLoggedMiddleware.js
+++ cristyle/src/middlewares/application/userLoggedMiddleware.js
@@ -1,20 +1,28 @@
 const db = require ("../../database/models");
 
 function userLoggedMiddleware (req,res,next){
+    // Ponemos la variable isLogged en false por defecto, en caso que 
+    // haya alguien logueado se cambiará a true.
     res.locals.isLogged = false;
 
-    // Check cookies
+    // Revisamos si hay cookies (es decir, si el usuario marcó "recordarme" la última
+    // vez que inició sesion y aun no cerró sesion).
     let emailInCookie = "";
     if (req.cookies.email) emailInCookie = req.cookies.email;
+    // Intentamos de buscar en la DB al usuario (emailInCookie tendrá el valor "" o el email guardado en cookies)
     db.User.findOne({where:
         {
             email: emailInCookie
         }
     }).then((userFromCookie) => {
+        // Si habia cookies del cliente, esto nos devolvería el cliente, y lo guardamos en session.
         if (userFromCookie) {
             req.session.userLogged = userFromCookie;
         }
-        // Check if someone is logged
+        // Si hay un usuario logueado, ya sea por las cookies del código posterior, o porque acaba 
+        // de loguearse en el método login del controlador (que tambien guarda en session al usuario)
+        // nos encargamos de guardarlo en locals para poder acceder a esta información en cualquier
+        // parte del proyecto
         if(req.session && req.session.userLogged){
             res.locals.isLogged = true;
             res.locals.userLogged = req.session.userLogged;
@@ -24,5 +32,4 @@ function userLoggedMiddleware (req,res,next){
         console.log(error)
     })
 }
-
 module.exports = userLoggedMiddleware;
