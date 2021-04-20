6ba4c477196f6eebc1940974b86da752cf649987
dogui7 <guido.maimone7@gmail.com>
2021-04-20 16:08:05 -0300

    Agregados comentarios en routes > products.js

--- cristyle/src/routes/products.js
+++ cristyle/src/routes/products.js
@@ -3,40 +3,43 @@ const router = express.Router();
 
 const productsController = require ("../controllers/productsController");
 
-//Middlewares
+/************************ Middlewares de ruta nativos e importados ************************/
+
+// Express-validator para validar los formularios de creacion de producto 
+// y edicion de producto, respectivamente
 const validateProductsCreate = require("../middlewares/routes/products/expressValidatorProductsCreate");
 const validateProductsEdit = require("../middlewares/routes/products/expressValidatorProductsEdit");
-const authMiddleware = require("../middlewares/routes/users/authMiddleware");
+// Multer para poder guardar archivos (imagen del producto)
 const upload = require("../middlewares/routes/products/multerProducts");
 
-//GET ONE PRODUCT
+/************************ Middlewares de ruta hechos por nosotros ************************/
 
+// Checkea que el usuario esté logueado. Utilizarlo para evitar que un usuario NO logueado
+// acceda a sitios como el carrito, y lo lleve a la vista de logueo.
+const authMiddleware = require("../middlewares/routes/users/authMiddleware");
+
+
+// GET ONE PRODUCT
 router.get("/detalle/:id", productsController.detail);
 
-//CART
-
+// CART
 router.get("/carrito",authMiddleware,productsController.cart);
 
-//PRODUCT EDIT
-
+// PRODUCT EDIT
 router.get ('/editar/:id', productsController.edit);
 router.put ('/:id', upload.single ('productImage'), validateProductsEdit, productsController.update);
 
-//PRODUCT CREATION
-
+// PRODUCT CREATION
 router.get ('/agregar', productsController.create);
 router.post ('/agregar', upload.single ('productImage'), validateProductsCreate, productsController.store);
 
-//SHOW ALL
-
+// SHOW ALL
 router.get('/todos', productsController.showAll);
 
-//SHOW FILTERED
-
+// SHOW FILTERED
 router.get('/todos/:filter', productsController.showFiltered);
 
-//DELETE ONE PRODUCT
-
+// DELETE ONE PRODUCT
 router.delete('/eliminar/:id', productsController.delete);
 
 module.exports = router;
