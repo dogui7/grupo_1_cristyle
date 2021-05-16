5d31a076ccdea4b2a5b271ff6bd5063e1f558f55
francoAssaneo <francoassaneo@gmail.com>
2021-05-16 19:42:36 -0300

    Completado JS

---
+++ cristyle/public/js/validationEditProfile.js
@@ -0,0 +1,99 @@
+window.onload=function(){
+    let form = document.querySelector('#formEditProfile');
+  
+    form.onsubmit = function(evento) {
+      // Si hay errores, evitamos mandar el formulario. Caso contrario se envía
+      if (!validateRegisterForm()) {
+        evento.preventDefault()
+      }else{
+        fomulario.submit()
+      }
+    }
+  
+    function validateRegisterForm() {
+      // Con destructuring separamos el formulario en variables individuales
+      let { name,lastname,image,} = form.elements
+      
+      // Checkeamos en funciones individuales cada campo
+      if (!validateName(name)) return false;
+      if (!validateName(lastname)) return false;
+      if (!validateImage(image)) return false;
+      return true;
+    }
+  
+    // Comento esta función, el resto son similares solo que cambia qué se verifica.
+    function validateName(name) {
+      // En la vista hay un elemento <p> al final de cada campo, donde se pondrán los errores. Lo guardamos en la variable error
+      let error = document.getElementById('nameErrorJs');
+      // Le ponemos la clase "error" por defecto
+      error.classList.add('error');
+  
+      // Si no se escribió nombre
+      if (name.value.length == 0) {
+        error.innerHTML = 'Debes completar el nombre del usuario';
+        return false;
+      }
+  
+      // Si el nombre no tiene al menos 5 caracteres
+      if (name.value.length < 5){
+        error.innerHTML = 'El nombre del usuario debe tener al menos 5 caracteres';
+        name.focus();
+        return false;
+      } 
+  
+      // En el caso de que no haya errores, borramos lo que tenga escrito y le quitamos la clase "error"
+      error.innerHTML = '';
+      error.classList.remove('error');
+      return true;
+    }
+
+    function validateName(lastname) {
+        // En la vista hay un elemento <p> al final de cada campo, donde se pondrán los errores. Lo guardamos en la variable error
+        let error = document.getElementById('lastNameErrorJs');
+        // Le ponemos la clase "error" por defecto
+        error.classList.add('error');
+    
+        
+        if (lastname.value.length == 0) {
+          error.innerHTML = 'Debes completar el apellido del usuario';
+          return false;
+        }
+    
+        
+        if (lastname.value.length < 5){
+          error.innerHTML = 'El apellido del usuario debe tener al menos 5 caracteres';
+          lastname.focus();
+          return false;
+        } 
+    
+        // En el caso de que no haya errores, borramos lo que tenga escrito y le quitamos la clase "error"
+        error.innerHTML = '';
+        error.classList.remove('error');
+        return true;
+      }
+  
+    function validateImage(image) {
+      let error = document.getElementById('imageErrorJs');
+      error.classList.add('error');
+  
+      // Si no se subio un archivo
+      if (image.value.length == 0) {
+        error.innerHTML = 'Debes subir una imagen de usuario';
+        return false;
+      }
+  
+      // Si no tiene una extensión permitida
+      let acceptedExtensions = ['jpeg', 'jpg', 'gif', 'png', 'JPEG', 'JPG', 'GIF', 'PNG'];
+      let parts = image.value.split('.');
+      let extension = parts[parts.length-1];
+      if (!acceptedExtensions.includes(extension)){
+        error.innerHTML = 'Las extensiones de archivo permitidas son ' + acceptedExtensions.join(', ');
+        return false;
+      }
+  
+      error.innerHTML = "";
+      error.classList.remove('error');
+      return true;   
+    }
+  
+}
--- cristyle/public/js/validationLogin.js
+++ cristyle/public/js/validationLogin.js
@@ -1,36 +1,70 @@
-/* const formulario = this.document.getElementById('formularioLogin');
-
-const inputs = this.document.querySelectorAll ('#formularioLogin input');
-
-const expresiones = {
-    password: /^.{8,20}$/, 
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+window.onload=function(){
+    let form = document.querySelector('#formularioLogin');
+  
+    form.onsubmit = function(evento) {
+      // Si hay errores, evitamos mandar el formulario. Caso contrario se envía
+      if (!validateRegisterForm()) {
+        evento.preventDefault()
+      }else{
+        fomulario.submit()
+      }
+    }
+  
+    function validateRegisterForm() {
+      // Con destructuring separamos el formulario en variables individuales
+      let {email,password} = form.elements
+      
+      // Checkeamos en funciones individuales cada campo
+      if (!validateName(email)) return false;
+      if (!validateImage(password)) return false;
+      return true;
+    }
+  
+    // Comento esta función, el resto son similares solo que cambia qué se verifica.
+    function validateName(email) {
+      // En la vista hay un elemento <p> al final de cada campo, donde se pondrán los errores. Lo guardamos en la variable error
+      let error = document.getElementById('emailErrorJs');
+      // Le ponemos la clase "error" por defecto
+      error.classList.add('error');
+  
+      // Si no se escribió email
+      if (email.value.length == 0) {
+        error.innerHTML = 'Debes completar el email del usuario';
+        return false;
+      }
+  
+      // Si el email no tiene al menos 5 caracteres
+      if (email.value.length < 5){
+        error.innerHTML = 'El email debe tener al menos 5 caracteres';
+        email.focus();
+        return false;
+      } 
+  
+      // En el caso de que no haya errores, borramos lo que tenga escrito y le quitamos la clase "error"
+      error.innerHTML = '';
+      error.classList.remove('error');
+      return true;
+    }
+  
+    function validateImage(password) {
+      let error = document.getElementById('passwordErrorJs');
+      error.classList.add('error');
+  
+      // Si no se completa la contraseña
+      if (password.value.length == 0) {
+        error.innerHTML = 'Debe completar la contraseña';
+        return false;
+      }
+      
+      if (password.value.length < 8) {
+        error.innerHTML = 'La contraseña debe contener al menos 8 caracteres';
+        return false;
+      }
+    
+  
+      error.innerHTML = "";
+      error.classList.remove('error');
+      return true;   
+    }
+  
 };
-
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "email":
-            if (expresiones.correo.test(e.target.value)){
-                document.querySelector ('.campo-texto label').classList.remove ('campo-texto-error ');
-            } else {
-                document.querySelector ('.campo-texto label').classList.add ('campo-texto-error ');
-            }
-        break;
-
-        case "password":
-
-        break;
-    }  
-}
-
-inputs.forEach ((input)=> {
-    input.addEventListener ('keyup' , validarFormulario)
-    input.addEventListener ('blur' , validarFormulario)
-});
-
-formulario.addEventListener ('submit', (e) => {
-    e.preventDefault();
-});
-
- */
